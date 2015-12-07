e59ae257018962535d06ec7a96d9ff7499bb6eb2
Jack Fransham <jack.fransham@cuvva.co>
2015-12-07 09:18:33 +0000

    Add benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+#![feature(test)]
+
 #[macro_use]
 extern crate nom;
+extern crate test;
 
 #[macro_use]
 mod macros;
@@ -15,14 +18,26 @@ use raw_bsp_parsers::*;
  * All numbers are little-endian *
  *********************************/
 
-fn main() {
-    let b = include_bytes!(
-        "../assets/simple-dm5.bsp"
-    );
+pub const SIMPLE_DM5: &'static [u8] = include_bytes!(
+    "../assets/simple-dm5.bsp"
+);
 
-    match parse_raw_bsp(b) {
+fn main() {
+    match parse_raw_bsp(SIMPLE_DM5) {
         Done(_, _) => println!("Success!"),
         Incomplete(n) => println!("Incomplete: {:?}", n),
         Error(_)   => println!("Failed :("),
+    }
+}
+
+mod test_main {
+    use test::Bencher;
+    use super::raw_bsp_parsers;
+
+    #[bench]
+    pub fn bench_raw_bsp(b: &mut Bencher) {
+        b.iter(|| {
+            raw_bsp_parsers::parse_raw_bsp(super::SIMPLE_DM5);
+        });
     }
 }
